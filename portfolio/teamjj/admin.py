4e0380fa7628d1d9eeb6077e8de36fe7070e8c12
kimseolhwa <esistkalt@naver.com>
2018-03-14 15:35:10 +0900

    view changed

--- portfolio/teamjj/admin.py
+++ portfolio/teamjj/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Skill, User, Project
 
 # Register your models here.
+
+admin.site.register(Skill)
+admin.site.register(User)
+admin.site.register(Project)
--- portfolio/teamjj/models.py
+++ portfolio/teamjj/models.py
@@ -1,22 +1,53 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Skill(models.Model) :
+
+
+	skillProficiency = (
+
+	    ('1', 'Freshman'),
+
+	    ('2', 'Sophomore'),
+
+	    ('3', 'Junior'),
+
+	    ('4', 'Senior'),
+
+	    ('5', 'Graduate'),
+
+	)
+
+	skill_profiency = models.CharField(max_length=1, choices=skillProficiency)
 	skill_name = models.CharField(max_length=100)
-	skill_proficiency = models.IntegerField(default=0)
 	skill_text = models.CharField(max_length=5000)
+
+	def __str__(self):
+		return self.skill_name
 
 class User(models.Model) :
 	user_name = models.CharField(max_length=100)
 	user_type = models.CharField(max_length=100)
 	user_age = models.IntegerField()
 	user_phone = models.CharField(max_length=100)
-	user_skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
+	user_skills = models.ManyToManyField(Skill)
+
+	def __str__(self):
+		return self.user_name
 
 class Project(models.Model) :
-    project_contributer = models.ForeignKey(User, on_delete=models.CASCADE)
-    project_skills = models.ForeignKey(Skill, on_delete=models.CASCADE)
+    project_contributer = models.ManyToManyField(User)
+    project_skills = models.ManyToManyField(Skill)
     project_name = models.CharField(max_length=200)
     project_text = models.CharField(max_length=5000)
     project_link = models.CharField(max_length=200) 
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+    	return self.project_name
+
+    def was_published_this_year(self):
+    	return self.pub_date >= timezone.now() - datetime.timedelta(days=365)
