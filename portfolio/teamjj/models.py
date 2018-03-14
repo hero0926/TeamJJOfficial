191b0002dfe4c3c700a12497a28e7fbcf807220c
kimseolhwa <esistkalt@naver.com>
2018-03-14 16:58:10 +0900

    basic projects view

--- portfolio/teamjj/models.py
+++ portfolio/teamjj/models.py
@@ -32,22 +32,23 @@ class User(models.Model) :
 	user_name = models.CharField(max_length=100)
 	user_type = models.CharField(max_length=100)
 	user_age = models.IntegerField()
-	user_phone = models.CharField(max_length=100)
 	user_skills = models.ManyToManyField(Skill)
+	user_phone = models.CharField(max_length=100)
 
 	def __str__(self):
 		return self.user_name
 
 class Project(models.Model) :
-    project_contributer = models.ManyToManyField(User)
-    project_skills = models.ManyToManyField(Skill)
-    project_name = models.CharField(max_length=200)
-    project_text = models.CharField(max_length=5000)
-    project_link = models.CharField(max_length=200) 
-    pub_date = models.DateTimeField('date published')
 
-    def __str__(self):
-    	return self.project_name
+	project_skills = models.ManyToManyField(Skill)
+	project_contributer = models.ManyToManyField(User)
 
-    def was_published_this_year(self):
-    	return self.pub_date >= timezone.now() - datetime.timedelta(days=365)
+	project_name = models.CharField(max_length=200)
+	project_text = models.CharField(max_length=5000)
+	project_link = models.CharField(max_length=200) 
+	pub_date = models.DateTimeField('date published')
+	def __str__(self):
+		return self.project_name
+
+	def was_published_this_year(self):
+		return self.pub_date >= timezone.now() - datetime.timedelta(days=365)
--- portfolio/teamjj/urls.py
+++ portfolio/teamjj/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "teamjj"
 urlpatterns = [
+ 	# ex: /teamjj/
     path('', views.index, name='index'),
+    # ex: /teamjj/user_name/
+    path('<user_name>/', views.user_detail, name='user_detail'),
+    # ex: /teamjj/projects/1/
+    path('projects/<int:id>/', views.project_detail, name='project_detail'),
+  
 ]
