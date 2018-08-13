4207326649c9307a83b0e2f1250d91a8a1f28528
kimseolhwa <esistkalt@naver.com>
2018-08-13 11:17:49 +0900

    using go not python

--- portfolio/teamjj/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Skill, User, Project
-
-# Register your models here.
-
-admin.site.register(Skill)
-admin.site.register(User)
-admin.site.register(Project)
